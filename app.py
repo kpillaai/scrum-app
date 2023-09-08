ee071eed9a71fffb1297b14fc45b9db3e7cf06be
mhoa0013 <mhoa0013@student.monash.edu>
2023-09-08 20:50:53 +1000

    edit button works

--- app.py
+++ app.py
@@ -22,6 +22,16 @@ def post_remove(index):
     session.modified = True
     return redirect(url_for('backlog'))
 
+@app.route('/edit/<int:index>',methods=['GET','POST'])
+def edit(index):
+    tasks = session['tasks_db'][index]
+    if request.method == 'POST':
+        session['tasks_db'][index] = request.form['task']
+        session.modified = True
+        return redirect(url_for('backlog'))
+    else:
+        return render_template('edit.html',task=tasks, index=index)
+
 @app.route('/backlog/')
 def backlog():
     if not 'tasks_db' in session:
