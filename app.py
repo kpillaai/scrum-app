899f5dc4be389a9b6afbbdbf384b06c8f40359d5
AlexHofmann <ghof0001@student.monash.edu>
2023-09-10 18:42:05 +1000

    Added basic login functionality

--- app.py
+++ app.py
@@ -38,6 +38,15 @@ def backlog():
         session['tasks_db'] = []
     return render_template('backlog.html', tasks_db=session["tasks_db"])
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
 if __name__ == "__main__":
     app.run(debug=True)
