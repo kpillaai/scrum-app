ef45cebfe9e348a1938d1ba3f48f7e5aa7f5a5e0
Krishna <kman0030@student.monash.edu>
2023-09-13 15:28:47 +1000

    updated user class to be be able to create database table
    updated user class to be be able to create database User table. Currently using the email as the "username"

--- models/user.py
+++ models/user.py
@@ -1,3 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
 from models.task import Task, db
 from enum import Enum
 
@@ -5,36 +6,11 @@ class RoleType(Enum):
     ADMIN = 'Admin'
     MEMBER = 'Member'
     
-class User:
-    next_id = 1
-
-    def __init__(self, name: str, role: RoleType, email: str, phone_number: str, password: str):
-        self.name = name
-        self.id = User.next_id  # unique id for each User
-        User.next_id += 1
-        self.role = role  # from RoleType, either ADMIN or MEMBER
-        self.email = email
-        self.phone_number = phone_number
-        self.task_dict = {}  # dict used to store tasks this user has contributed to, key is task.id, value is hours
-        self.password = password
-
-    def add_task(self, task: Task) -> None:
-        """
-        Adds a task to this User and adds it to the User's task_dict
-        :param task: Task the User is working on
-        :return: None
-        """
-        if self.task_dict[task.id] is not None:
-            pass
-        else:
-            self.task_dict[task.id] = 0
-
-    def update_hours(self, task: Task, hours: int) -> None:
-        """
-        Update the number of hours the User has spent working on the assigned Task
-        :param task: Task the User is assigned to
-        :param hours: number of hours the User contributed
-        :return: None
-        """
-        self.task_dict[task.id] += hours
-
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False) # unique id for each user
+    name = db.Column(db.String)
+    role = db.Column(db.Enum(RoleType), default=RoleType.MEMBER)  # from RoleType, either Admin or Member
+    email = db.Column(db.String, unique=True)
+    phone_number = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    # what tasks are assigned to the user
