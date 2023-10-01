a4007edd6618943b8a748717e9017ec0150f36c1
Jason Sakolkraisorn <jsak0004@student.monash.edu>
2023-10-01 14:53:09 +1100

    - Updated Assignee to directly reference users (id) in user db & renamed home to dashboard along with heading size changes

--- models/task.py
+++ models/task.py
@@ -11,13 +11,12 @@ class TaskStatus(Enum):
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False) # unique id for each task
     name = db.Column(db.String)
-    description = db.Column(db.String)
+    description = db.Column(db.String, default="")
     priority = db.Column(db.Integer, unique=False)
     status = db.Column(db.Enum(TaskStatus), default=TaskStatus.TODO) # from TaskStatus, either TODO, IN_PROGRESS or DONE
     estimated_effort = db.Column(db.Integer)
     start_date = db.Column(db.DateTime(timezone=True))
     due_date = db.Column(db.DateTime(timezone=True))
-    # assignee =  db.Column(db.Integer, db.ForeignKey('user.id')) # just one User per task or multiple?
-    assignee = db.Column(db.String) # just one User per task or multiple?
+    assignee =  db.Column(db.Integer, db.ForeignKey('user.id')) # ForeignKey = reference to another db table (id)
     hours_taken = db.Column(db.Integer)
     
